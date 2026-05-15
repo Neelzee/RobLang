2d79ec69e5912121b675f18974c0ef506051d487
Neelzee <nilsien2001@gmail.com>
2026-05-15 21:12:42 +0200

    tests: Added let-decl parse test

--- src/RobLang.Tests/HelloWorld.Tests.fs
+++ src/RobLang.Tests/HelloWorld.Tests.fs
@@ -9,9 +9,18 @@ let Setup () =
   ()
 
 [<Test>]
-let Test1 () =
+let HelloWorld () =
   let p = parse $"print(\"Hello, World!\")"
   match p with
   | Ok r ->
     Assert.That(r.Head, Is.EqualTo (FnCall ("print", [String "Hello, World!"])))
+  | Error err -> Assert.Fail $"Failed: {err}"
+
+
+[<Test>]
+let LetDecl () =
+  let p = parse $"let foo = \"Hello, World!\""
+  match p with
+  | Ok r ->
+    Assert.That(r.Head, Is.EqualTo (VarDecl ("foo", String "Hello, World!")))
   | Error err -> Assert.Fail $"Failed: {err}"
