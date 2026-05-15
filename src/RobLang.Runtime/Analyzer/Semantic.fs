0e0af078f8dc2eef381a8cfdafa28bf606eb3843
Neelzee <nilsien2001@gmail.com>
2026-05-15 14:56:10 +0200

    feat(Runtime): Added Expression check

--- src/RobLang.Runtime/Analyzer/Semantic.fs
+++ src/RobLang.Runtime/Analyzer/Semantic.fs
@@ -12,13 +12,35 @@ type ScopeKind =
 
 type SymbolTableError =
   | VarShadowing of string
+  | MissingVariableDeclaration of string
   | MissingFunctionDeclaration of string
   | MissingArgument of string
   | InvalidUseOfBreakStmt
   | InvalidUseOfReturnStmt
 
-let buildSymbolTable (program : Program) : Result<SymbolTable, SymbolTableError> =
-  Ok mkSymbolTable
+let rec checkExpr
+  (expr : Expr)
+  (st : SymbolTable)
+  : Result<SymbolTable, SymbolTableError> =
+  match expr with  
+  | Array xs ->
+    List.fold
+      (fun st cur ->
+        match st with
+        | Ok st' -> checkExpr cur st'
+        | Error err -> Error err
+      ) (Ok st) xs
+  | Var id ->
+    if Option.isNone (lookupVariable id st) then
+      Error (MissingVariableDeclaration id)
+    else
+      Ok st
+  | Call(id, _) ->
+    if Option.isNone (lookupFunctionId id st) then
+      Error (MissingFunctionDeclaration id)
+    else
+      Ok st
+  | _ -> Ok st
 
 let rec checkStmt
   (sks : ScopeKind list)
@@ -26,7 +48,9 @@ let rec checkStmt
   (stmt : Stmt)
   : Result<SymbolTable, SymbolTableError> =
   match stmt, sks with  
-  | VarDecl(id, expr), _ -> addVariable st id expr |> Ok
+  | VarDecl(id, expr), _ ->
+    addVariable st id expr
+    |> checkExpr expr
   | FnDecl(id, parms, block), _ -> addFunction st (id, parms) block |> Ok  
   | If(_, body), _ ->
     List.fold
@@ -37,7 +61,7 @@ let rec checkStmt
       )
       (Ok (mkSymbolTableInner st))
       body
-  | While(_, body), _ ->
+  | While(cond, body), _ ->
     List.fold
       (fun acc cur ->
         match acc with
@@ -46,7 +70,8 @@ let rec checkStmt
       )
       (Ok (mkSymbolTableInner st))
       body
-  | IfElse(_, branch1, branch2), _ ->
+    |> Result.bind (checkExpr cond)
+  | IfElse(cond, branch1, branch2), _ ->
     let result1 =
       List.fold
         (fun acc cur ->
@@ -66,10 +91,10 @@ let rec checkStmt
         (Ok (mkSymbolTableInner st))
         branch2
     match result1, result2 with
-    | Ok st, Ok _ -> Ok st
+    | Ok st, Ok _ -> checkExpr cond st
     | Error err, _ -> Error err
     | _, Error err -> Error err
-  | For(_, _, body), _ ->
+  | For(_, iter, body), _ ->
     List.fold
       (fun acc cur ->
         match acc with
@@ -78,6 +103,7 @@ let rec checkStmt
       )
       (Ok (mkSymbolTableInner st))
       body
+    |> Result.bind (checkExpr iter)
   | FnCall(id, args), _ ->
     match lookupFunctionId id st with
     | Some (parms, body) ->
@@ -90,13 +116,14 @@ let rec checkStmt
           |> List.zip parms
         let fnSt: Result<SymbolTable, SymbolTableError> = 
           List.fold (fun st' ((id, opt), expr) ->
-            match st' with
-            | Ok st'' ->
+            st'
+            |> Result.bind (fun st'' ->
               match expr, opt with
-              | Some v, _ -> addVariable st'' id v |> Ok
-              | _, Some o -> addVariable st'' id o |> Ok
+              | Some e, _ | _, Some e ->
+                addVariable st'' id e
+                |> checkExpr e
               | None, None -> Error (MissingArgument id)
-            | Error err -> Error err
+            )
           ) (Ok (mkSymbolTableInner st)) parameters
         List.fold
           (fun acc cur ->
@@ -116,8 +143,15 @@ let rec checkStmt
       Ok st
     else
       Error InvalidUseOfBreakStmt
-  | Return _, sks ->
+  | Return expr, sks ->
     if List.contains FunctionScope sks then
-      Ok st
+      match expr with      
+      | Some v -> checkExpr v st
+      | None -> Ok st
     else
       Error InvalidUseOfReturnStmt
+ 
+let buildSymbolTable (program : Program) : Result<SymbolTable, SymbolTableError> =
+  List.fold (fun st cur ->
+    Result.bind (fun st' -> checkStmt [OuterScope] st' cur) st
+  ) (Ok mkSymbolTable) program
